d54c6e4a1b6c49f1b1fe5697670ec997c3fced65
Arester <temultik@gmail.com>
2020-04-20 15:43:39 +0300

    update(source code): encrypt correct working

--- viger_code.py
+++ viger_code.py
@@ -1,18 +1,22 @@
 abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
 input_txt = 'Attack at dawn'
-code_word = 'Lemon'
-output_txt = []
-
-
 input_txt = input_txt.lower()
 input_txt = input_txt.replace(' ','')
-code_input = []
+x = len(input_txt)
 
+code_word = 'Lemon'
 code_word = code_word.lower()
 
+for i in code_word:
+	while len(code_word) < x:
+		l = 0
+		code_word = code_word+code_word[l:]
+		l += 1
+code_word = code_word[0:x]
+
+code_input = []
 res = []
 
 for j in input_txt:
@@ -25,17 +29,11 @@ for i in code_word:
 	abc_tail = abc[:x:]
 	abc_new = [abc_head + abc_tail]
 	abc_new = [i for i in abc_new for i in i]
-	print(code_input)
-	print(abc_new)
 	for i in code_input:
 		s = abc_new[i]
-		print(s)
 		res.append(s)
-		print(res)
 		code_input.remove(i)
-		print(code_input)
 		break
 
-# print(output_txt)
-
-"""LXFOPVEFRNHR"""
+res = ''.join(res)
+print(res)
